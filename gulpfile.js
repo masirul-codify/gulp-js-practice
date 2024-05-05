9c69374b096d8b639cdedd5febc463a33fa10a48
Masirul Islam <masirulislam80@gmail.com>
2024-05-05 17:09:01 +0600

    add: template

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,54 @@ const gulp = require('gulp');
 const { src, dest, task } = gulp;
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
+// const babel = require('gulp-babel');
+// const imagemin  = require('gulp-imagemin');
 
 task('sass', () => {
   return src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(dest('./dist/css/'))
+    .pipe(dest('./dist/assets/css/'))
     .pipe(browserSync.stream());
 });
 
+// task('image_minify', () => {
+//   return src('./src/asets/images/**/*') 
+//     .pipe(imagemin([
+//       gifsicle({interlaced: true}),
+//       mozjpeg({quality: 75, progressive: true}),
+//       optipng({optimizationLevel: 5}),
+//       svgo({
+//         plugins: [
+//           {
+//             name: 'removeViewBox',
+//             active: true
+//           },
+//           {
+//             name: 'cleanupIDs',
+//             active: false
+//           }
+//         ]
+//       })
+//     ]))
+//     .pipe(dest('./dist/assets/images/'))
+//     .pipe(browserSync.stream());
+// });
+
+task('assets', () => {
+  return src('./src/assets/**/*')
+    .pipe(dest('./dist/assets'))
+    .pipe(browserSync.stream()); 
+});
+
 task('js', () => {
-  return src('./src/js/**/*.js')
-    .pipe(dest('./dist/js'))
+  return src('./src/assets/js/**/*')
+  //   .pipe(babel({
+  //     presets: ['@babel/env']
+  // }))
+    .pipe(dest('./dist/assets/js'))
     .pipe(browserSync.stream()); // Reload browser after JS changes
 });
+
 
 task('html', () => {
   return src('./src/*.html')
@@ -29,13 +64,14 @@ task('server', () => {
     }
   });
 
-  // Watch HTML and Sass files for changes
+  
   gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
   gulp.watch('./src/*.html', gulp.series('html'));
-  gulp.watch('./src/js/**/*.js', gulp.series('js'));
+  gulp.watch('./src/assets/js/**/*', gulp.series('js'));
+  gulp.watch('./src/assets/**/*', gulp.series('assets'));
 });
 
-task('build', gulp.series('sass', 'js', 'html', 'server'));
+task('build', gulp.series('sass', 'html',  'js', 'assets', 'server'));
 
 gulp.task('default', gulp.series('build'));
 
@@ -51,3 +87,13 @@ gulp.task('default', gulp.series('build'));
 
 // exports.default = defaultTask;
 
+
+
+
+
+
+
+/*eslint-disable*/
+
+//lossy compression
+// https://gist.github.com/LoyEgor/e9dba0725b3ddbb8d1a68c91ca5452b5
--- src/js/main.js
+++
@@ -1,1 +0,0 @@
-console.log('Hello again from main 3');
